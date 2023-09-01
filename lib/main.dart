ab0e53b975ab28736b202676319ee1ebe06f0e2c
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-08-31 16:20:56 -0800

    Add validation for email and password

--- lib/main.dart
+++ lib/main.dart
@@ -24,29 +24,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _signInKey = GlobalKey();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final RegExp emailValid = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+_/=>^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Twitter Clone")),
       body: Form(
+        key: _signInKey,
         child: Column(
           children: <Widget>[
             TextFormField(
               controller: _emailController,
               keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(hintText: "Enter an Email"),
+              decoration: const InputDecoration(
+                hintText: "Enter an Email",
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter an email";
+                }
+                if (!emailValid.hasMatch(value)) {
+                  return "Please enter a valid email";
+                }
+                return null;
+              },
             ), //email
             TextFormField(
               controller: _passwordController,
               obscureText: true,
-              decoration: const InputDecoration(hintText: "Enter a Password"),
+              decoration: const InputDecoration(
+                hintText: "Enter a Password",
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a password";
+                }
+                if (value.length < 6) {
+                  return "Password must be at least 6 characters";
+                }
+                return null;
+              },
             ), //password
             ElevatedButton(
               onPressed: () {
-                debugPrint("Email: ${_emailController.text}");
-                debugPrint("Password: ${_passwordController.text}");
+                if (_signInKey.currentState!.validate()) {
+                  debugPrint("Email: ${_emailController.text}");
+                  debugPrint("Password: ${_passwordController.text}");
+                }
               },
               child: const Text("Submit"),
             )
